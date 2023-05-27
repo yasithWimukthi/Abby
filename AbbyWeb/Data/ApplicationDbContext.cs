8ae3a0f1e568e63c622b2d42121be2b322467bd2
yasith <wimukthiyasith@gmail.com>
2023-05-27 23:56:38 +0530

    create db context

--- AbbyWeb/Data/ApplicationDbContext.cs
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -5,6 +5,12 @@ namespace AbbyWeb.Data
 {
 	public class ApplicationDbContext : DbContext
 	{
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
 		public DbSet<Category> Category { get; set; }
 	}
 }
